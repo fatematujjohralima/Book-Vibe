213380978208d09f7db3af59f373c0280751a4a5
fatematujjohra <fatematujjohralima2@gmail.com>
2026-06-19 17:22:20 +0600

    Implement dynamic routes for every single book

--- src/Pages/Book/Book.jsx
+++ src/Pages/Book/Book.jsx
@@ -1,29 +1,41 @@
 import React from 'react';
 import { IoMdStarHalf } from "react-icons/io";
+import { Link } from 'react-router';
 
 const Book = ({data}) => {
     console.log(data);
-    const {bookName,author,image,review,rating,category}=data
+    const {bookName,bookId,author,image,review,rating,category,tags,
+yearOfPublishing
+}=data
     return (
-            <div className="card bg-base-100 w-96 shadow-sm border p-6">
+           <Link to={`/bookdetails/${bookId}`}>
+            <div className="card bg-base-100 w-96 shadow-sm border p-6 shadow-2xl">
   <figure className='p-4 bg-gray-200 w-2/3 mx-auto '>
     <img className='h-[166px]'
       src={image}
       alt="Shoes" />
   </figure>
   <div className="card-body">
+    <div className="flex justify-center gap-10">
+        {
+            tags.map(tag => <button>{tag}</button>)
+        }
+    </div>
     <h2 className="card-title">
       {bookName}
-      {/* <div className="badge badge-secondary">NEW</div> */}
+      <div className="badge badge-secondary">{
+yearOfPublishing
+}</div>
     </h2>
     <h3>{author}</h3>
-    <p>{review}</p>
+    <p className="overflow-hidden text-ellipsis line-clamp-3">{review}</p>
+    <div className='border-t-1 border-dashed mb-3'></div>
     <div className="card-actions justify-end">
       <div className="badge badge-outline">{category}</div>
       <div className="badge badge-outline">{rating} <IoMdStarHalf /></div>
     </div>
   </div>
-</div>
+</div></Link>
         
     );
 };
--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -3,6 +3,8 @@ import { createBrowserRouter } from "react-router";
 import Root from '../Pages/Root/Root';
 import ErrorPage from '../Pages/ErrorPage/ErrorPage';
 import Home from '../Pages/Home/Home';
+import About from '../Pages/About/About';
+import BookDetails from '../Pages/BookDetails/BookDetails';
 
 export const router = createBrowserRouter([
   {
@@ -15,6 +17,14 @@ export const router = createBrowserRouter([
             loader: () => fetch('https://raw.githubusercontent.com/ProgrammingHero1/boi-poka-Book-Vibe-Resources/refs/heads/main/data/booksData.json'),
             path: '/',
             Component: Home
+        },
+        {
+          path: '/about',
+          Component: About
+        },
+        {
+          path: '/bookdetails/:id',
+          Component: BookDetails
         }
     ]
   },
