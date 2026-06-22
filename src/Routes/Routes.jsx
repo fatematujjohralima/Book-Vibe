d933627b171cccd909ba6d8772259394e36d874a
fatematujjohra <fatematujjohralima2@gmail.com>
2026-06-22 16:33:52 +0600

    implement read list functionality with localStorage, sorting options, and react-tabs UI

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -1,10 +1,11 @@
-import React from 'react';
+import React from "react";
 import { createBrowserRouter } from "react-router";
-import Root from '../Pages/Root/Root';
-import ErrorPage from '../Pages/ErrorPage/ErrorPage';
-import Home from '../Pages/Home/Home';
-import About from '../Pages/About/About';
-import BookDetails from '../Pages/BookDetails/BookDetails';
+import Root from "../Pages/Root/Root";
+import ErrorPage from "../Pages/ErrorPage/ErrorPage";
+import Home from "../Pages/Home/Home";
+import About from "../Pages/About/About";
+import BookDetails from "../Pages/BookDetails/BookDetails";
+import ReadList from "../Pages/ReadList/ReadList";
 
 export const router = createBrowserRouter([
   {
@@ -12,21 +13,35 @@ export const router = createBrowserRouter([
     Component: Root,
     errorElement: <ErrorPage></ErrorPage>,
     children: [
-        {
-            index: true,
-            loader: () => fetch('https://raw.githubusercontent.com/ProgrammingHero1/boi-poka-Book-Vibe-Resources/refs/heads/main/data/booksData.json'),
-            path: '/',
-            Component: Home
-        },
-        {
-          path: '/about',
-          Component: About
-        },
-        {
-          path: '/bookdetails/:id',
-          loader: () => fetch('https://raw.githubusercontent.com/ProgrammingHero1/boi-poka-Book-Vibe-Resources/refs/heads/main/data/booksData.json'),
-          Component: BookDetails
-        }
-    ]
+      {
+        index: true,
+        loader: () =>
+          fetch(
+            "https://raw.githubusercontent.com/ProgrammingHero1/boi-poka-Book-Vibe-Resources/refs/heads/main/data/booksData.json",
+          ),
+        path: "/",
+        Component: Home,
+      },
+      {
+        path: "/about",
+        Component: About,
+      },
+      {
+        path: "/readlist",
+        loader: () =>
+          fetch(
+            "https://raw.githubusercontent.com/ProgrammingHero1/boi-poka-Book-Vibe-Resources/refs/heads/main/data/booksData.json",
+          ),
+        Component: ReadList,
+      },
+      {
+        path: "/bookdetails/:id",
+        loader: () =>
+          fetch(
+            "https://raw.githubusercontent.com/ProgrammingHero1/boi-poka-Book-Vibe-Resources/refs/heads/main/data/booksData.json",
+          ),
+        Component: BookDetails,
+      },
+    ],
   },
 ]);
